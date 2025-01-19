297ac9d6f7ff84faaa57ec5dc52f76eb780ff45c
Mert <114191420+lumiboi@users.noreply.github.com>
2025-01-19 17:39:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,44 @@ def index():
         )
     return render_template("index.html")
 
+@app.route("/follow", methods=["GET", "POST"])
+def follow_index():
+    if request.method == "POST":
+        username = request.form["username"].lower()
+        following, followers = get_follow_data(username)
+        difference_list = set(following) - set(followers)
+        return render_template("follow-result.html", username=username, difference_list=difference_list)
+    return render_template("followerboxd.html")
+
+def get_follow_data(username):
+    following = []
+    followers = []
+
+    def get_users(source, page_name):
+        persons = source.find_all("div", attrs={"class": "person-summary"})
+        for i in persons:
+            if page_name == "following":
+                following.append(i.find("h3").find("a")["href"])
+            else:
+                followers.append(i.find("h3").find("a")["href"])
+
+    def connect_page(page_name):
+        page_num = 1
+        while True:
+            url = f"https://letterboxd.com/{username}/{page_name}/page/{page_num}/"
+            source = BeautifulSoup(requests.get(url).content, "lxml")
+            if f'<a class="next" href="/{username}/{page_name}/' in str(source):
+                get_users(source, page_name)
+                page_num += 1
+            else:
+                get_users(source, page_name)
+                break
+
+    for page_name in ("following", "followers"):
+        connect_page(page_name)
+
+    return following, followers
+
 def get_watched_movies(username):
     watched_movies = []
 
